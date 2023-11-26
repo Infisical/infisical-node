eea13b09cd22e22b3ee23734458b692c2903197b
Tristan Blackwell <32044892+TristanBlackwell@users.noreply.github.com>
2023-11-26 22:07:59 +0000

    test: test folder actions

--- tests/client/InfisicalClient.test.ts
+++ tests/client/InfisicalClient.test.ts
@@ -1,158 +1,177 @@
-require('dotenv').config();
-import { describe, expect } from '@jest/globals';
-import InfisicalClient from '../../src';
+require("dotenv").config();
+import { describe, expect } from "@jest/globals";
+import InfisicalClient from "../../src";
 
-describe('InfisicalClient', () => {
-    let client: InfisicalClient;
-    beforeAll(async () => {
-        client = new InfisicalClient({
-            token: process.env.INFISICAL_TOKEN!,
-            tokenJson: process.env.INFISICAL_TOKEN_JSON!,
-            siteURL: process.env.SITE_URL!,
-            debug: true
-        });
-        await client.createSecret('KEY_ONE', 'KEY_ONE_VAL');
-        await client.createSecret('KEY_ONE', 'KEY_ONE_VAL_PERSONAL', {
-            type: "personal",
-            environment: "dev",
-            path: "/"
-        });
-        await client.createSecret('KEY_TWO', 'KEY_TWO_VAL');
-    }, 10000);
+describe("InfisicalClient", () => {
+  let client: InfisicalClient;
+  beforeAll(async () => {
+    client = new InfisicalClient({
+      token: process.env.INFISICAL_TOKEN!,
+      tokenJson: process.env.INFISICAL_TOKEN_JSON!,
+      siteURL: process.env.SITE_URL!,
+      debug: true,
+    });
+    await client.createSecret("KEY_ONE", "KEY_ONE_VAL");
+    await client.createSecret("KEY_ONE", "KEY_ONE_VAL_PERSONAL", {
+      type: "personal",
+      environment: "dev",
+      path: "/",
+    });
+    await client.createSecret("KEY_TWO", "KEY_TWO_VAL");
+    await client.createFolder("FOLDER_ONE");
+  }, 10000);
 
-    afterAll(async () => {
-        await client.deleteSecret('KEY_ONE');
-        await client.deleteSecret('KEY_TWO');
-        await client.deleteSecret('KEY_THREE');
-    }, 10000);
+  afterAll(async () => {
+    await client.deleteSecret("KEY_ONE");
+    await client.deleteSecret("KEY_TWO");
+    await client.deleteSecret("KEY_THREE");
+    await client.deleteFolder("FOLDER_ONE");
+  }, 10000);
 
-    it('get overriden personal secret', async () => {
-        const secret = await client.getSecret('KEY_ONE', {
-            type: "personal",
-            environment: "dev",
-            path: "/"
-        });
-
-        expect(secret.secretName).toBe('KEY_ONE');
-        expect(secret.secretValue).toBe('KEY_ONE_VAL_PERSONAL');
-        expect(secret.type).toBe('personal');
+  it("get overriden personal secret", async () => {
+    const secret = await client.getSecret("KEY_ONE", {
+      type: "personal",
+      environment: "dev",
+      path: "/",
     });
 
-    it('get shared secret specified', async () => {
-        const secret = await client.getSecret('KEY_ONE', {
-            type: 'shared',
-            environment: "dev",
-            path: "/"
-        });
+    expect(secret.secretName).toBe("KEY_ONE");
+    expect(secret.secretValue).toBe("KEY_ONE_VAL_PERSONAL");
+    expect(secret.type).toBe("personal");
+  });
 
-        expect(secret.secretName).toBe('KEY_ONE');
-        expect(secret.secretValue).toBe('KEY_ONE_VAL');
-        expect(secret.type).toBe('shared');
+  it("get shared secret specified", async () => {
+    const secret = await client.getSecret("KEY_ONE", {
+      type: "shared",
+      environment: "dev",
+      path: "/",
     });
 
-    it('get shared secret', async () => {
-        const secret = await client.getSecret('KEY_TWO');
+    expect(secret.secretName).toBe("KEY_ONE");
+    expect(secret.secretValue).toBe("KEY_ONE_VAL");
+    expect(secret.type).toBe("shared");
+  });
 
-        expect(secret.secretName).toBe('KEY_TWO');
-        expect(secret.secretValue).toBe('KEY_TWO_VAL');
-        expect(secret.type).toBe('shared');
+  it("get shared secret", async () => {
+    const secret = await client.getSecret("KEY_TWO");
+
+    expect(secret.secretName).toBe("KEY_TWO");
+    expect(secret.secretValue).toBe("KEY_TWO_VAL");
+    expect(secret.type).toBe("shared");
+  });
+
+  it("create shared secret", async () => {
+    const secret = await client.createSecret("KEY_THREE", "KEY_THREE_VAL");
+
+    expect(secret.secretName).toBe("KEY_THREE");
+    expect(secret.secretValue).toBe("KEY_THREE_VAL");
+    expect(secret.type).toBe("shared");
+  });
+
+  it("create personal secret", async () => {
+    await client.createSecret("KEY_FOUR", "KEY_FOUR_VAL");
+    const secretPersonal = await client.createSecret("KEY_FOUR", "KEY_FOUR_VAL_PERSONAL", {
+      type: "personal",
+      environment: "dev",
+      path: "/",
     });
 
-    it('create shared secret', async () => {
-        const secret = await client.createSecret('KEY_THREE', 'KEY_THREE_VAL');
+    expect(secretPersonal.secretName).toBe("KEY_FOUR");
+    expect(secretPersonal.secretValue).toBe("KEY_FOUR_VAL_PERSONAL");
+    expect(secretPersonal.type).toBe("personal");
+  });
 
-        expect(secret.secretName).toBe('KEY_THREE');
-        expect(secret.secretValue).toBe('KEY_THREE_VAL');
-        expect(secret.type).toBe('shared');
+  it("update shared secret", async () => {
+    const secret = await client.updateSecret("KEY_THREE", "FOO");
+
+    expect(secret.secretName).toBe("KEY_THREE");
+    expect(secret.secretValue).toBe("FOO");
+    expect(secret.type).toBe("shared");
+  });
+
+  it("update personal secret", async () => {
+    const secret = await client.updateSecret("KEY_FOUR", "BAR", {
+      type: "personal",
+      environment: "dev",
+      path: "/",
     });
 
-    it('create personal secret', async () => {
-        await client.createSecret('KEY_FOUR', 'KEY_FOUR_VAL');
-        const secretPersonal = await client.createSecret('KEY_FOUR', 'KEY_FOUR_VAL_PERSONAL', {
-            type: "personal",
-            environment: "dev",
-            path: "/"
-        });
+    expect(secret.secretName).toBe("KEY_FOUR");
+    expect(secret.secretValue).toBe("BAR");
+    expect(secret.type).toBe("personal");
+  });
 
-        expect(secretPersonal.secretName).toBe('KEY_FOUR');
-        expect(secretPersonal.secretValue).toBe('KEY_FOUR_VAL_PERSONAL');
-        expect(secretPersonal.type).toBe('personal');
+  it("delete personal secret", async () => {
+    const secret = await client.deleteSecret("KEY_FOUR", {
+      type: "personal",
+      environment: "dev",
+      path: "/",
     });
 
-    it('update shared secret', async () => {
-        const secret = await client.updateSecret('KEY_THREE', 'FOO');
+    expect(secret.secretName).toBe("KEY_FOUR");
+    expect(secret.secretValue).toBe("BAR");
+    expect(secret.type).toBe("personal");
+  });
 
-        expect(secret.secretName).toBe('KEY_THREE');
-        expect(secret.secretValue).toBe('FOO');
-        expect(secret.type).toBe('shared');
-    });
+  it("delete shared secret", async () => {
+    const secret = await client.deleteSecret("KEY_FOUR");
 
-    it('update personal secret', async () => {
-        const secret = await client.updateSecret('KEY_FOUR', 'BAR', {
-            type: "personal",
-            environment: "dev",
-            path: "/"
-        });
+    expect(secret.secretName).toBe("KEY_FOUR");
+    expect(secret.secretValue).toBe("KEY_FOUR_VAL");
+    expect(secret.type).toBe("shared");
+  });
 
-        expect(secret.secretName).toBe('KEY_FOUR');
-        expect(secret.secretValue).toBe('BAR');
-        expect(secret.type).toBe('personal');
-    });
+  it("attach all to process.env", async () => {
+    // note: getAllSecrets returns dupliate shared + personal ones at the moment,
+    // should it default to personal?
+    // const secretBundles = await client.getAllSecrets({
+    //     environment: "dev",
+    //     path: "/",
+    //     attachToProcessEnv: true,
+    //     includeImports: false
+    // });
+    // can't test this because both key are same, only type is diff
+    // secretBundles.forEach((secretBundle) => {
+    //     expect(process.env[secretBundle.secretName]).toBe(secretBundle.secretValue);
+    // });
+  });
 
-    it('delete personal secret', async () => {
-        const secret = await client.deleteSecret('KEY_FOUR', {
-            type: "personal",
-            environment: "dev",
-            path: "/"
-        });
+  it("get folder", async () => {
+    const folders = await client.listFolders();
 
-        expect(secret.secretName).toBe('KEY_FOUR');
-        expect(secret.secretValue).toBe('BAR');
-        expect(secret.type).toBe('personal');
-    });
+    expect(folders.length).toBeGreaterThan(0);
 
-    it('delete shared secret', async () => {
-        const secret = await client.deleteSecret('KEY_FOUR');
+    const folder = folders.find((f) => f.name === "FOLDER_ONE");
+    expect(folder?.name).toEqual("FOLDER_ONE");
+  });
 
-        expect(secret.secretName).toBe('KEY_FOUR');
-        expect(secret.secretValue).toBe('KEY_FOUR_VAL');
-        expect(secret.type).toBe('shared');
-    });
+  it("create folder", async () => {
+    const folder = await client.createFolder("FOLDER_TWO");
+    expect(folder?.name).toEqual("FOLDER_TWO");
+  });
 
-    it('attach all to process.env', async () => {
-        // note: getAllSecrets returns dupliate shared + personal ones at the moment,
-        // should it default to personal?
-        // const secretBundles = await client.getAllSecrets({
-        //     environment: "dev",
-        //     path: "/",
-        //     attachToProcessEnv: true,
-        //     includeImports: false
-        // });
+  it("update folder", async () => {
+    const folder = await client.updateFolder("FOLDER_TWO", "FOLDER_THREE");
+    expect(folder?.name).toEqual("FOLDER_THREE");
+  });
 
-        // can't test this because both key are same, only type is diff 
-        // secretBundles.forEach((secretBundle) => {
-        //     expect(process.env[secretBundle.secretName]).toBe(secretBundle.secretValue);
-        // });
-    });
+  it("delete folder", async () => {
+    const folders = await client.deleteFolder("FOLDER_THREE");
+    expect(folders?.length).toBeGreaterThan(0);
 
-    it('encrypt/decrypt symmetric', () => {
-        const plaintext = 'The quick brown fox jumps over the lazy dog';
+    const deleted = folders?.find((f) => f.name === "FOLDER_THREE");
+    expect(deleted?.name).toEqual("FOLDER_THREE");
+  });
 
-        const key = client.createSymmetricKey();
+  it("encrypt/decrypt symmetric", () => {
+    const plaintext = "The quick brown fox jumps over the lazy dog";
 
-        const {
-            ciphertext,
-            iv,
-            tag
-        } = client.encryptSymmetric(plaintext, key);
+    const key = client.createSymmetricKey();
 
-        const cleartext = client.decryptSymmetric(
-            ciphertext,
-            key,
-            iv,
-            tag
-        );
+    const { ciphertext, iv, tag } = client.encryptSymmetric(plaintext, key);
 
-        expect(plaintext).toBe(cleartext);
-    });
+    const cleartext = client.decryptSymmetric(ciphertext, key, iv, tag);
+
+    expect(plaintext).toBe(cleartext);
+  });
 });
